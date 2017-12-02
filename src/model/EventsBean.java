31d1efa405e7db96f6be7197c8d51b8bc68a410e
Mark Luis Gabriel V. Ticzon <markvticzon@gmail.com>
2017-12-02 13:52:02 +0800

    2nd commit

---
+++ src/model/EventsBean.java
@@ -0,0 +1,11 @@
+package model;
+
+public class EventsBean {
+	
+	String venue;
+	String date;
+	String time;
+	
+	
+
+}
---
+++ src/model/Webinar.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Webinar {
+	
+	String registrationKey;
+	String date;
+	String time;
+	String rate;
+	String earlyBirdRate;
+	
+	public String getRegistrationKey() {
+		return registrationKey;
+	}
+	public void setRegistrationKey(String registrationKey) {
+		this.registrationKey = registrationKey;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getRate() {
+		return rate;
+	}
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+	public String getEarlyBirdRate() {
+		return earlyBirdRate;
+	}
+	public void setEarlyBirdRate(String earlyBirdRate) {
+		this.earlyBirdRate = earlyBirdRate;
+	}
+	
+	
+	
+	
+
+}
