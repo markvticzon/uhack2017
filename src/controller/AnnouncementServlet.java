cbb75c3cc32f0a7919369a725a4e6e38a7120d04
Mark Luis Gabriel V. Ticzon <markvticzon@gmail.com>
2017-12-02 21:02:44 +0800

    YES!

--- src/controller/AnnouncementServlet.java
+++ src/controller/AnnouncementServlet.java
@@ -1,8 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,29 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/dashboard.html")
-public class DashboardServlet extends HttpServlet {
+/**
+ * Servlet implementation class AnnouncementServlet
+ */
+@WebServlet("/announcement.html")
+public class AnnouncementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	static Connection con;
-	static PreparedStatement pstmt;
-
+       
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-
 		HttpSession sessionUser  = (HttpSession) request.getSession(false).getAttribute("student");
 		System.out.println(sessionUser);
-		if(request.getSession(false) == null){
-			response.sendRedirect("index.jsp");
+		if(sessionUser == null){
+			response.sendRedirect("index.html");
 		}else {
 			
-			response.sendRedirect("dashboard.jsp");
-
 		}
-	
 	}
 
 }
--- src/controller/LogoutServlet.java
+++ src/controller/LogoutServlet.java
@@ -28,6 +28,6 @@ public class LogoutServlet extends HttpServlet {
 		if(sessionUser!= null){
 			sessionUser.invalidate();
 	}
-		response.sendRedirect("index.html");
+		response.sendRedirect("index.jsp");
 	}
 }
